9c9a5c558ed2e94a52dff0bbcea49db5bd243e77
Joshua Mellon <joshuamellon1@gmail.com>
2023-03-30 20:22:59 +0100

    Added model and database connection for recipes

---
+++ webapi/Models/RecipeBookDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace webapi.Models
+{
+    public class RecipeBookDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
