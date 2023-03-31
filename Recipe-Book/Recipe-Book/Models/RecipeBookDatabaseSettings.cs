d6001b94ecc3061ea6b36b5bea725b235d6aed6b
Joshua Mellon <joshuamellon1@gmail.com>
2023-03-31 19:11:19 +0100

    stupid stupid stupid stupid

---
+++ Recipe-Book/Recipe-Book/Models/RecipeBookDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipe_Book.Models
+{
+    public class RecipeBookDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string RecipesCollectionName { get; set; } = null!;
+    }
+}
