8b191abde0fd73177df7f6fa66e60f95f064cc79
Joshua Mellon <joshuamellon1@gmail.com>
2023-04-01 22:02:37 +0100

    Both got auth0 working and then not working

--- Recipe-Book/Recipe-Book/Program.cs
+++ Recipe-Book/Recipe-Book/Program.cs
@@ -1,7 +1,27 @@
 using Recipe_Book.Models;
 using Recipe_Book.Services;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Recipe_Book.Support;
+using System.Net;
+
+using Microsoft.IdentityModel.Logging;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+//To use MVC we have to explicitly declare we are using it. Doing so will prevent a System.InvalidOperationException.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
 
 // Add services to the container.
 builder.Services.Configure<RecipeBookDatabaseSettings>(
@@ -12,7 +32,6 @@ builder.Services.AddSingleton<RecipeService>();
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
 
 builder.Services.AddControllers()
     .AddJsonOptions(
@@ -22,7 +41,18 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure the HTTP request pipeline.
+builder.Services.ConfigureSameSiteNoneCookies();
 var app = builder.Build();
+
+app.UseCookiePolicy();
+
+app.UseStaticFiles();
+app.UseCookiePolicy();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,8 +62,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
