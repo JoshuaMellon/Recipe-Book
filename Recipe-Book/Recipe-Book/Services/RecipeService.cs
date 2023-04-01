d6d3ad42ba3e7154249484477959754e2be9f102
Joshua Mellon <joshuamellon1@gmail.com>
2023-04-01 17:25:56 +0100

    Create recipe service tasks and commenting

--- Recipe-Book/Recipe-Book/Services/RecipeService.cs
+++ Recipe-Book/Recipe-Book/Services/RecipeService.cs
@@ -1,4 +1,5 @@
-﻿using Recipe_Book.Models;
+﻿// import namespaces for the model class, miccro extension, and the mongo driver
+using Recipe_Book.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
@@ -6,17 +7,35 @@ namespace Recipe_Book.Services
 {
     public class RecipeService
     {
+        // Declare the recipes collection within mongo
         private readonly IMongoCollection<Recipe> _recipesCollection;
 
+        // Declare the recipe service
         public RecipeService (IOptions<RecipeBookDatabaseSettings> recipeBookDatabaseSettings)
         {
+            // Connect to the mongo clinent using the declared conenction string within app settings
             var mongoClient = new MongoClient (recipeBookDatabaseSettings.Value.ConnectionString);
 
+            // Get the database from the mongo connection
             var mongoDatabase = mongoClient.GetDatabase(recipeBookDatabaseSettings.Value.DatabaseName);
-
+            
+            // Get the collection from the mongo connection
             _recipesCollection = mongoDatabase.GetCollection<Recipe>(recipeBookDatabaseSettings.Value.RecipesCollectionName);
         }
-
+        
+        // Declare task for getting the databases within the collection and convert to a list
         public async Task<List<Recipe>> GetAsync() => await _recipesCollection.Find(_ => true).ToListAsync();
+        
+        // Declare task for getting all the 'Recipes' within the colleciton and convert to a list
+        public async Task<Recipe?> GetAsync(string id) => await _recipesCollection.Find(x => x.ID == id).FirstOrDefaultAsync();
+
+        // Declare task for creating a new recipe within the collection
+        public async Task CreateAsync(Recipe newRecipe) => await _recipesCollection.InsertOneAsync(newRecipe);
+
+        // Declare task for updating a  recipe within the collectio
+        public async Task UpdatedAsync(string id, Recipe updatedRecipe) => await _recipesCollection.ReplaceOneAsync(x => x.ID == id, updatedRecipe);
+
+        // Declare task for removing a recipe within the collectio
+        public async Task RemoveAsync(string id) => await _recipesCollection.DeleteOneAsync(x => x.ID == id);
     }
 }
