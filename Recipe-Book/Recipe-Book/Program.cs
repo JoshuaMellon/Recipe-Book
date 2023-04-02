ace411a0f4b4ce8a291792bd434dc7f7078d2ae0
Joshua Mellon <joshuamellon1@gmail.com>
2023-04-02 13:56:01 +0100

    Temporary removal of login system
    was having a few issue with my auth0 endpoints and decided to postpone full set up till app is working

--- Recipe-Book/Recipe-Book/Program.cs
+++ Recipe-Book/Recipe-Book/Program.cs
@@ -1,27 +1,8 @@
 using Recipe_Book.Models;
 using Recipe_Book.Services;
-using Auth0.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Recipe_Book.Support;
-using System.Net;
-
-using Microsoft.IdentityModel.Logging;
-
 
 var builder = WebApplication.CreateBuilder(args);
-
-//To use MVC we have to explicitly declare we are using it. Doing so will prevent a System.InvalidOperationException.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddAuth0WebAppAuthentication(options =>
-{
-    options.Domain = builder.Configuration["Auth0:Domain"];
-    options.ClientId = builder.Configuration["Auth0:ClientId"];
-});
 
 // Add services to the container.
 builder.Services.Configure<RecipeBookDatabaseSettings>(
@@ -30,12 +11,12 @@ builder.Services.Configure<RecipeBookDatabaseSettings>(
 // Add RecipeService to DI for contructor injection for the consuming classes - https://mongodb.github.io/mongo-csharp-driver/2.14/reference/driver/connecting/#re-use
 builder.Services.AddSingleton<RecipeService>();
 
-// Add services to the container.
-
-
 builder.Services.AddControllers()
     .AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+//To use MVC we have to explicitly declare we are using it. Doing so will prevent a System.InvalidOperationException.
+builder.Services.AddControllersWithViews();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -45,13 +26,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSameSiteNoneCookies();
 var app = builder.Build();
 
-app.UseCookiePolicy();
-
-app.UseStaticFiles();
-app.UseCookiePolicy();
-
 app.UseRouting();
-app.UseAuthentication();
+
 app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
